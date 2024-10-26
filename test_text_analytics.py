438107846db0ed2a30636c715b06c477052dab6a
camilamoyac <cami.csmc@gmail.com>
2024-10-26 16:38:31 -0300

    test functions first commit

---
+++ test_text_analytics.py
@@ -0,0 +1,21 @@
+from text_analytics import read_file, format_string, string_to_list, \
+    list_to_word_amount_list, remove_stopwords, sort_word_amount_list
+import pytest
+
+def test_read_file():
+    pass
+
+def test_format_string():
+    pass
+
+def test_string_to_list():
+    pass
+
+def test_list_to_word_amount_list():
+    pass
+
+def test_remove_stopwords():
+    pass
+
+def test_sort_word_amount_list():
+    pass
